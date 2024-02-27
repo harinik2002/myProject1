f08c9e8461c583079f99324ef7192617d86aa52b
harinik2002 <harinikathiravan2002@gmail.com>
2024-02-27 20:45:03 +0530

    api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Route;//
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+
+/*
+use App\Http\Controllers\ConstantController;
+use App\Http\Controllers\EmployeeInsertController;
+use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+
+Route::get('constant',[ConstantController::class,'index']);
+
+Route::get('register',[RegisterController::class,'register']);
+
+Route::post('store',[RegisterController::class,'store']);
+
+
+Route::get('/', function () {
+    return view('home');
 });
+
+Route::get('login',[LoginController::class,'login']);
+
+
+
+Route::post('authenticate',[LoginController::class,'authenticate']);
+
+Route::get('logout',[EmployeeInsertController::class,'logout']);
+
+Route::get('success', function () {
+    return view('registersuccess');
+});
+
+
+
+Route::get('/newemployee', [EmployeeInsertController::class, 'insertForm']);
+Route::post('/create', [EmployeeInsertController::class, 'insert']);
+Route::get('/employees', [EmployeeInsertController::class, 'showEmployee']);
+Route::get('/edit/{id}', [EmployeeInsertController::class, 'edit']);
+Route::post('/edit/{id}', [EmployeeInsertController::class, 'update']);
+
+
+?>
